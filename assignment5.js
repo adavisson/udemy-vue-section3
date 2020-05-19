953bb8f47368670817b56a5a813c57fd8d9e47e9
Andrew Davisson <akdavisson4@gmail.com>
2020-05-18 22:49:52 -0600

    completed assignment 5

---
+++ assignment5.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#assignment5',
+  data: {
+    show: true,
+    sports: ['basketball', 'soccer', 'golf', 'football', 'croquet'],
+    lotr: {
+      title: 'Lord of the Rings',
+      author: 'J. R. R. Tolkien',
+      books: '3',
+    },
+    toughLoop: {
+      name: 'TESTOBJECT',
+      data: [1.67, 1.33, 0.98, 2.21],
+    },
+  },
+})
